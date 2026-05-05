97a7eb6014fdb00016aa0a43656cc074c2d2a64d
toko0715 <strongsitojheremy@gmail.com>
2026-05-05 11:08:09 -0500

    finish

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,11 @@ export const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 );
