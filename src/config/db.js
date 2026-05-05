b83a84c381d5a68b8b922190937e934152103607
toko0715 <strongsitojheremy@gmail.com>
2026-05-05 11:21:28 -0500

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,8 @@ export const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
     dialectOptions: {
       ssl: {
         require: true,
